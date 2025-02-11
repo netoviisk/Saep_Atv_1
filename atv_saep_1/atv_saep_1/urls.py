367f0c3d476082b7e5eece75f2c77b450850fb2c
Juarez Neto <juaresnetof@gmail.com>
2025-02-11 13:17:42 -0300

    Delete atv_saep_1 directory

--- atv_saep_1/atv_saep_1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from website import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('cadastro_usuario/', views.cadastro_usuario, name='cadastro_usuario'),
-    path('cadastro_tarefa/', views.cadastro_tarefa, name='cadastro_tarefa'),
-]
